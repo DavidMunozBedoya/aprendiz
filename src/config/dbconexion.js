48de0460d921d4aba0b057e4b99c902f2b1b0853
davidMuñoz <munozd50@gmail.com>
2025-07-30 12:43:46 -0500

    correccion en el archivo dbConexion.js para permitir multiples conexiones a la base de datos con mysql.createPool

--- src/config/dbconexion.js
+++ src/config/dbconexion.js
@@ -1,16 +1,19 @@
 import mysql from "mysql2/promise";
 
-const dbconn = await mysql.createConnection({
+const dbconn = await mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_BASE,
   password: process.env.DB_PWD,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-try {
+/* try {
   await dbconn.connect();
 } catch (err) {
   console.log(err);
-}
+} */
 
 export default dbconn;
