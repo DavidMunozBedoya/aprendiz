ed64154b0067fd4b30696f0995d73d10094cb958
davidMuñoz <munozd50@gmail.com>
2025-07-30 12:48:52 -0500

    opciones dbconexion

--- src/config/dbconexion.js
+++ src/config/dbconexion.js
@@ -1,19 +1,26 @@
 import mysql from "mysql2/promise";
 
-const dbconn = await mysql.createPool({
+/* const dbconn = await mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_BASE,
   password: process.env.DB_PWD,
   waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  connectionLimit: 10,   // se puede ajustar la carga
+  queueLimit: 0,
+}); */
+
+const dbconn = await mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_BASE,
+  password: process.env.DB_PWD,
 });
 
-/* try {
+try {
   await dbconn.connect();
 } catch (err) {
   console.log(err);
-} */
+}
 
 export default dbconn;
